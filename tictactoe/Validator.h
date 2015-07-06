c172937553cf3c2944ee4a3f787bff4fada41e96
calvached <calvache.diana@gmail.com>
2015-07-06 03:36:31 -0500

    Validates user input for board size and opponent selection.

---
+++ tictactoe/Validator.h
@@ -0,0 +1,16 @@
+#ifndef tictactoe_Validator_h
+#define tictactoe_Validator_h
+
+#import "IOttt.h"
+
+@interface Validator : NSObject
+
+@property (nonatomic) IOttt *io;
+
+- (id)initWithIO:(IOttt *)io;
+- (int)promptForBoardSize;
+- (NSString *)promptForOpponent;
+
+@end
+
+#endif
