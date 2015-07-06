c8dd14a6bab16d0329f34e63500dbb0661f0e042
calvached <calvache.diana@gmail.com>
2015-07-06 03:58:48 -0500

    Integrate Validator into Config

--- specs/ConfigTest.m
+++ specs/ConfigTest.m
@@ -2,6 +2,7 @@
 #import <XCTest/XCTest.h>
 #import "Config.h"
 #import "IOMock.h"
+#import "Validator.h"
 
 @interface ConfigTest : XCTestCase
 
@@ -24,11 +25,11 @@
     [testInputs addObject:@"3"];
     [testInputs addObject:@"e"];
     ioMock.inputs = testInputs;
-    Config *config = [[Config alloc] initWithIO:ioMock];
+    Validator *validator = [[Validator alloc] initWithIO:ioMock];
+    Config *config = [[Config alloc] initWithIO:ioMock andValidator:validator];
 
     NSString *className = NSStringFromClass([[config getSettings][@"board"] class]);
 
-    XCTAssertTrue([ioMock.outputs containsObject:@"Choose a board size: "], @"prompts the user for a board size.");
     XCTAssertEqualObjects(className, @"Board", @"creates a board.");
 }
 
@@ -39,11 +40,11 @@
     [testInputs addObject:@"3"];
     [testInputs addObject:@"e"];
     ioMock.inputs = testInputs;
-    Config *config = [[Config alloc] initWithIO:ioMock];
+    Validator *validator = [[Validator alloc] initWithIO:ioMock];
+    Config *config = [[Config alloc] initWithIO:ioMock andValidator:validator];
 
     NSString *className = NSStringFromClass([[config getSettings][@"playerTwo"] class]);
 
-    XCTAssertTrue([ioMock.outputs containsObject:@"Choose an opponent ('e' for Easy): "], @"prompts the user to choose an opponent.");
     XCTAssertEqualObjects(className, @"EasyAi", @"creates an easy AI player.");
 }
 
@@ -54,7 +55,8 @@
     [testInputs addObject:@"3"];
     [testInputs addObject:@"e"];
     ioMock.inputs = testInputs;
-    Config *config = [[Config alloc] initWithIO:ioMock];
+    Validator *validator = [[Validator alloc] initWithIO:ioMock];
+    Config *config = [[Config alloc] initWithIO:ioMock andValidator:validator];
 
     NSString *className = NSStringFromClass([[config getSettings][@"playerOne"] class]);
 
@@ -68,11 +70,27 @@
     [testInputs addObject:@"3"];
     [testInputs addObject:@"e"];
     ioMock.inputs = testInputs;
-    Config *config = [[Config alloc] initWithIO:ioMock];
+    Validator *validator = [[Validator alloc] initWithIO:ioMock];
+    Config *config = [[Config alloc] initWithIO:ioMock andValidator:validator];
 
     NSString *className = NSStringFromClass([[config getSettings][@"io"] class]);
 
     XCTAssertEqualObjects(className, @"IOMock", @"creates IO.");
+}
+
+- (void)testAssignsValidatorToSettings {
+    IOMock *ioMock = [[IOMock alloc] init];
+    ioMock.outputs = [NSMutableArray array];
+    NSMutableArray *testInputs = [NSMutableArray array];
+    [testInputs addObject:@"3"];
+    [testInputs addObject:@"e"];
+    ioMock.inputs = testInputs;
+    Validator *validator = [[Validator alloc] initWithIO:ioMock];
+    Config *config = [[Config alloc] initWithIO:ioMock andValidator:validator];
+
+    NSString *className = NSStringFromClass([[config getSettings][@"validator"] class]);
+
+    XCTAssertEqualObjects(className, @"Validator", @"assigns validator to settings.");
 }
 
 @end
